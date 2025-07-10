408314fa174a32b3262d9cedcaa960dacdb2f932
ErickIgles <erick.almeida2125@gmail.com>
2025-07-10 11:40:44 -0300

    feat: cria modelo de usuário customizado.

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+
+
+class CustomUserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O e-mail deve ser informado.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('is_superuser precisa ser True')
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('is_staff precisa ser True')
+        return self._create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name='E-mail', unique=True)
+    first_name = models.CharField(verbose_name='Primeiro Nome', max_length=30, blank=True)
+    last_name = models.CharField(verbose_name='Sobrenome', max_length=30, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+    def __str__(self):
+        return self.email
 
