29c4443b3bc6f4675b9583ff698a8d860be66a75
ErickIgles <erick.almeida2125@gmail.com>
2025-07-10 19:45:41 -0300

    feat: adiciona CustomUser ao Django admin com campos personalizados e controle de permissões.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import CustomUser
 
 
+@admin.register(CustomUser)
+class UserAdmin(BaseUserAdmin):
+
+    model = CustomUser
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+    list_display_links = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Informações pessoais'), {'fields': ('first_name', 'last_name')}),
+        (('Permissões'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (('Datas importantes'), {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
