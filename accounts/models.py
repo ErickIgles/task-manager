6620839d5911212422d3989b4e58faaa8a87e42f
ErickIgles <erick.almeida2125@gmail.com>
2025-07-10 19:47:15 -0300

    fix: adiciona campos is_staff e is_superuser ao modelo CustomUser.

--- accounts/models.py
+++ accounts/models.py
@@ -36,6 +36,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(verbose_name='Primeiro Nome', max_length=30, blank=True)
     last_name = models.CharField(verbose_name='Sobrenome', max_length=30, blank=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
